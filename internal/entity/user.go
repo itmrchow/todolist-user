720406cc37540c8655f555eb2b2ef4db402f479f
Jeff Wu <itmrchow@gmail.com>
2025-03-17 23:25:11 +0800

    feat: add "HashPassword" in user entity

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,6 +1,9 @@
 package entity
 
 import (
+	"crypto/sha512"
+	"fmt"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -11,4 +14,12 @@ type User struct {
 	Name       string    `gorm:"size:20;not null"`
 	Email      string    `gorm:"uniqueIndex;size:255;not null"`
 	Password   string    `gorm:"size:255;not null"`
+}
+
+func (u *User) HashPassword() {
+
+	passwordBytes := sha512.Sum512([]byte(u.Password))
+	password := fmt.Sprintf("%x", passwordBytes)
+
+	u.Password = password
 }
