02ada41f7d90ea58fb4cb36ef903f56273488f9b
Jeff Wu <itmrchow@gmail.com>
2025-03-17 18:28:48 +0800

    feat: create users_repo , users table db action

---
+++ internal/repository/users_repo_impl.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+
+	"github.com/itmrchow/todolist-users/internal/entity"
+)
+
+var _ UsersRepository = &database{}
+
+type database struct {
+	conn *gorm.DB
+}
+
+func NewUsersRepository(conn *gorm.DB) UsersRepository {
+	return &database{
+		conn: conn,
+	}
+}
+
+func (d *database) Create(ctx context.Context, user *entity.User) error {
+	return d.conn.WithContext(ctx).Create(user).Error
+}
+
+func (d *database) Get(ctx context.Context, id uuid.UUID) (user *entity.User, err error) {
+	if err := d.conn.WithContext(ctx).Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+func (d *database) GetByEmail(ctx context.Context, email string) (user *entity.User, err error) {
+	if err := d.conn.WithContext(ctx).Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+func (d *database) Update(ctx context.Context, user *entity.User) error {
+	return d.conn.WithContext(ctx).Model(&entity.User{}).Where("id = ?", user.ID).Updates(user).Error
+}
+
+func (d *database) Delete(ctx context.Context, id uuid.UUID) error {
+	return d.conn.WithContext(ctx).Where("id = ?", id).Delete(&entity.User{}).Error
+}
---
+++ internal/repository/users_repo_interface.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/itmrchow/todolist-users/internal/entity"
+)
+
+type UsersRepository interface {
+	Create(ctx context.Context, user *entity.User) error
+	Get(ctx context.Context, id uuid.UUID) (*entity.User, error)
+	GetByEmail(ctx context.Context, email string) (*entity.User, error)
+	Update(ctx context.Context, user *entity.User) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
