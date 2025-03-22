4863f9179da60a0491c6e2867edca83e44d27cc8
Jeff Wu <itmrchow@gmail.com>
2025-03-22 22:32:10 +0800

    refactor: restructure unit tests, one function creates one suite.

--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.23.6
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
+	github.com/itmrchow/todolist-proto v0.0.0-20250322125151-3207beefe9ac
+	github.com/rs/zerolog v1.34.0
 	github.com/spf13/viper v1.20.0
 	github.com/stretchr/testify v1.10.0
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 )
@@ -18,6 +22,8 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
@@ -29,7 +35,9 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/service/user_service_impl_test.go
+++ internal/service/user_service_impl_test.go
@@ -2,218 +2,320 @@ package service
 
 import (
 	"context"
-	"crypto/sha512"
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
+	pb "github.com/itmrchow/todolist-proto/protobuf/user"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 
 	"github.com/itmrchow/todolist-users/internal/entity"
-	mErr "github.com/itmrchow/todolist-users/internal/errors"
 	"github.com/itmrchow/todolist-users/internal/repository"
 )
 
-func TestUserServiceImplSuite(t *testing.T) {
-	suite.Run(t, new(UserServiceImplTestSuite))
+func TestRegisterTestSuite(t *testing.T) {
+	suite.Run(t, new(RegisterTestSuite))
 }
 
-type UserServiceImplTestSuite struct {
+type RegisterTestSuite struct {
 	suite.Suite
-	userService  UserService
+	userService  pb.UserServiceServer
 	mockUserRepo *repository.MockUsersRepository
+	input        struct {
+		ctx context.Context
+		req *pb.RegisterRequest
+	}
 }
 
-func (s *UserServiceImplTestSuite) SetupTest() {
-
+func (s *RegisterTestSuite) SetupTest() {
 	userRepo := repository.NewMockUsersRepository(s.T())
 	s.mockUserRepo = userRepo
 	s.userService = NewUserService(userRepo)
 }
 
-func (s *UserServiceImplTestSuite) Test_userServiceImpl_RegisterUser() {
-	type args struct {
+func (s *RegisterTestSuite) Test_Register_ExistsByEmail_DbError() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.RegisterRequest{
+		Email:    "db_error@example.com",
+		Password: "password",
+		Name:     "test",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().ExistsByEmail(context.Background(), mock.Anything).Return(false, errors.New("db error"))
+
+	// execute
+	resp, err := s.userService.Register(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	rpcErr, ok := status.FromError(err)
+	s.Assert().True(ok)
+	s.Assert().Equal(codes.Internal, rpcErr.Code())
+
+}
+
+func (s *RegisterTestSuite) Test_Register_ExistsByEmail_EmailAlreadyExists() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.RegisterRequest{
+		Email:    "exist@example.com",
+		Password: "password",
+		Name:     "test",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().ExistsByEmail(context.Background(), mock.Anything).Return(true, nil)
+
+	// execute
+	resp, err := s.userService.Register(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	rpcErr, ok := status.FromError(err)
+	s.Assert().True(ok)
+	s.Assert().Equal(codes.AlreadyExists, rpcErr.Code())
+	s.Assert().Equal("email already exists", rpcErr.Message())
+
+}
+
+func (s *RegisterTestSuite) Test_Register_Create_DbError() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.RegisterRequest{
+		Email:    "test@example.com",
+		Password: "password",
+		Name:     "test",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().ExistsByEmail(context.Background(), s.input.req.Email).Return(false, nil)
+	s.mockUserRepo.EXPECT().Create(context.Background(), mock.MatchedBy(func(user *entity.User) bool {
+		return user.Email == s.input.req.Email &&
+			user.Password != s.input.req.Password &&
+			user.Name == s.input.req.Name &&
+			user.ID != uuid.Nil
+	})).Return(errors.New("db error"))
+
+	// execute
+	resp, err := s.userService.Register(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	rpcErr, ok := status.FromError(err)
+	s.Assert().True(ok)
+	s.Assert().Equal(codes.Internal, rpcErr.Code())
+}
+
+func (s *RegisterTestSuite) Test_Register_Success() {
+
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.RegisterRequest{
+		Email:    "test@example.com",
+		Password: "password",
+		Name:     "test",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().ExistsByEmail(context.Background(), "test@example.com").Return(false, nil)
+	s.mockUserRepo.EXPECT().Create(context.Background(), mock.MatchedBy(func(user *entity.User) bool {
+		return user.Email == s.input.req.Email &&
+			user.Password != s.input.req.Password &&
+			user.Name == s.input.req.Name &&
+			user.ID != uuid.Nil
+	})).Return(nil)
+
+	// execute
+	resp, err := s.userService.Register(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	s.Assert().Nil(err)
+}
+
+func TestLoginTestSuite(t *testing.T) {
+	suite.Run(t, new(LoginTestSuite))
+}
+
+type LoginTestSuite struct {
+	suite.Suite
+	userService  pb.UserServiceServer
+	mockUserRepo *repository.MockUsersRepository
+	input        struct {
 		ctx context.Context
-		req *RegisterReqDTO
-	}
-	tests := []struct {
-		name       string
-		args       args
-		mockFunc   func(m *repository.MockUsersRepository)
-		assertFunc func(err error)
-	}{
-		{
-			name: "db error",
-			args: args{
-				ctx: context.Background(),
-				req: &RegisterReqDTO{
-					Email:    "db_error@example.com",
-					Password: "password",
-					Name:     "test",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().ExistsByEmail(context.Background(), "db_error@example.com").Return(false, errors.New("db error"))
-			},
-			assertFunc: func(err error) {
-				s.Assert().Error(err)
-				s.Assert().ErrorIs(err, &mErr.Err500InternalServer)
-			},
-		},
-		{
-			name: "email exist",
-			args: args{
-				ctx: context.Background(),
-				req: &RegisterReqDTO{
-					Email:    "exist@example.com",
-					Password: "password",
-					Name:     "test",
-				},
-			},
-			mockFunc: func(mock *repository.MockUsersRepository) {
-				mock.EXPECT().ExistsByEmail(context.Background(), "exist@example.com").Return(true, nil)
-			},
-			assertFunc: func(err error) {
-				s.Assert().Error(err)
-				s.Assert().Equal("email already exists", err.Error())
-			},
-		},
-		{
-			name: "insert db fail",
-			args: args{
-				ctx: context.Background(),
-				req: &RegisterReqDTO{
-					Email:    "test@example.com",
-					Password: "password",
-					Name:     "test",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().ExistsByEmail(context.Background(), "test@example.com").Return(false, nil)
-				m.EXPECT().Create(context.Background(), mock.Anything).Return(errors.New("db error")).Times(1)
-			},
-			assertFunc: func(err error) {
-				s.Assert().Error(err)
-				s.Assert().Equal("internal server error", err.Error())
-			},
-		},
-		{
-			name: "success",
-			args: args{
-				ctx: context.Background(),
-				req: &RegisterReqDTO{
-					Email:    "test@example.com",
-					Password: "password",
-					Name:     "test",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().ExistsByEmail(context.Background(), "test@example.com").Return(false, nil)
-				m.EXPECT().Create(context.Background(), mock.MatchedBy(func(user *entity.User) bool {
-
-					passwordBytes := sha512.Sum512([]byte(user.Password))
-					password := fmt.Sprintf("%x", passwordBytes)
-
-					return user.Email == "test@example.com" && user.Password != password && user.Name == "test"
-
-				})).Return(nil).Times(1)
-			},
-			assertFunc: func(err error) {
-				s.Assert().NoError(err)
-			},
-		},
-	}
-	for _, tt := range tests {
-
-		s.Run(tt.name, func() {
-
-			tt.mockFunc(s.mockUserRepo)
-			err := s.userService.RegisterUser(tt.args.ctx, tt.args.req)
-			tt.assertFunc(err)
-		})
+		req *pb.LoginRequest
 	}
 }
 
-func (s *UserServiceImplTestSuite) Test_userServiceImpl_LoginUser() {
-	type args struct {
-		ctx context.Context
-		req *LoginReqDTO
-	}
-	tests := []struct {
-		name       string
-		args       args
-		mockFunc   func(m *repository.MockUsersRepository)
-		assertFunc func(resp *LoginRespDTO, err error)
-	}{
-		{
-			name: "db error",
-			args: args{
-				ctx: context.Background(),
-				req: &LoginReqDTO{
-					Email:    "db_error@example.com",
-					Password: "password",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().GetByEmailAndPassword(context.Background(), "db_error@example.com", mock.AnythingOfType("string")).Return(nil, errors.New("db error")).Times(1)
-			},
-			assertFunc: func(resp *LoginRespDTO, err error) {
-				s.Assert().ErrorIs(err, &mErr.Err500InternalServer)
-			},
-		},
-		{
-			name: "user not found",
-			args: args{
-				ctx: context.Background(),
-				req: &LoginReqDTO{
-					Email:    "not_found@example.com",
-					Password: "password",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().GetByEmailAndPassword(context.Background(), "not_found@example.com", mock.AnythingOfType("string")).Return(nil, gorm.ErrRecordNotFound).Times(1)
-			},
-			assertFunc: func(resp *LoginRespDTO, err error) {
-				s.Assert().ErrorIs(err, &mErr.Err400InvalidLoginInfo)
-			},
-		},
-		{
-			name: "success",
-			args: args{
-				ctx: context.Background(),
-				req: &LoginReqDTO{
-					Email:    "success@example.com",
-					Password: "password",
-				},
-			},
-			mockFunc: func(m *repository.MockUsersRepository) {
-				m.EXPECT().GetByEmailAndPassword(context.Background(), "success@example.com", mock.AnythingOfType("string")).Return(&entity.User{
-					ID:       uuid.New(),
-					Email:    "success@example.com",
-					Password: "password",
-					Name:     "test",
-				}, nil).Times(1)
-			},
-			assertFunc: func(resp *LoginRespDTO, err error) {
-				s.Assert().NoError(err)
-				s.Assert().Equal("success@example.com", resp.Email)
-				s.Assert().Equal("test", resp.Name)
-				s.Assert().NotEmpty(resp.Token)
-				s.Assert().NotEmpty(resp.ExpiresIn)
-			},
-		},
-	}
-
-	for _, tt := range tests {
-
-		s.Run(tt.name, func() {
-
-			tt.mockFunc(s.mockUserRepo)
-			resp, err := s.userService.LoginUser(tt.args.ctx, tt.args.req)
-			tt.assertFunc(resp, err)
-		})
-	}
+func (s *LoginTestSuite) SetupTest() {
+	userRepo := repository.NewMockUsersRepository(s.T())
+	s.mockUserRepo = userRepo
+	s.userService = NewUserService(userRepo)
 }
+
+func (s *LoginTestSuite) Test_Login_GetByEmailAndPassword_DBError() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.LoginRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().GetByEmailAndPassword(context.Background(), s.input.req.Email, mock.MatchedBy(func(password string) bool {
+		return password != s.input.req.Password
+	})).Return(nil, errors.New("db error"))
+
+	// execute
+	resp, err := s.userService.Login(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	rpcErr, ok := status.FromError(err)
+	s.Assert().True(ok)
+	s.Assert().Equal(codes.Internal, rpcErr.Code())
+}
+
+func (s *LoginTestSuite) Test_Login_UserNotFound() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.LoginRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().GetByEmailAndPassword(context.Background(), s.input.req.Email, mock.MatchedBy(func(password string) bool {
+		return password != s.input.req.Password
+	})).Return(nil, gorm.ErrRecordNotFound)
+
+	// execute
+	resp, err := s.userService.Login(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().Nil(resp)
+	rpcErr, ok := status.FromError(err)
+	s.Assert().True(ok)
+	s.Assert().Equal(codes.Unauthenticated, rpcErr.Code())
+}
+
+func (s *LoginTestSuite) Test_Login_Success() {
+	// input
+	s.input.ctx = context.Background()
+	s.input.req = &pb.LoginRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	userID := uuid.New()
+	user := &entity.User{
+		ID:       userID,
+		Email:    s.input.req.Email,
+		Name:     "test",
+		Password: "hashed_password",
+	}
+
+	// mock
+	s.mockUserRepo.EXPECT().GetByEmailAndPassword(context.Background(), s.input.req.Email, mock.MatchedBy(func(password string) bool {
+		return password != s.input.req.Password
+	})).Return(user, nil)
+
+	// execute
+	resp, err := s.userService.Login(s.input.ctx, s.input.req)
+
+	// assert
+	s.Assert().NotNil(resp)
+	s.Assert().Nil(err)
+	s.Assert().Equal(userID.String(), resp.Id)
+	s.Assert().Equal(user.Name, resp.Name)
+	s.Assert().Equal(user.Email, resp.Email)
+	s.Assert().NotEmpty(resp.Token)
+	s.Assert().NotNil(resp.ExpiresIn)
+}
+
+// func (s *UserServiceImplTestSuite) Test_userServiceImpl_Login() {
+// 	type args struct {
+// 		ctx context.Context
+// 		req *pb.LoginRequest
+// 	}
+// 	tests := []struct {
+// 		name       string
+// 		args       args
+// 		mockFunc   func(m *repository.MockUsersRepository)
+// 		assertFunc func(resp *pb.LoginResponse, err error)
+// 	}{
+// 		{
+// 			name: "db error",
+// 			args: args{
+// 				ctx: context.Background(),
+// 				req: &pb.LoginRequest{
+// 					Email:    "db_error@example.com",
+// 					Password: "password",
+// 				},
+// 			},
+// 			mockFunc: func(m *repository.MockUsersRepository) {
+// 				m.EXPECT().GetByEmailAndPassword(context.Background(), "db_error@example.com", mock.AnythingOfType("string")).Return(nil, errors.New("db error")).Times(1)
+// 			},
+// 			assertFunc: func(resp *pb.LoginResponse, err error) {
+// 				s.Assert().ErrorIs(err, &mErr.Err500InternalServer)
+// 			},
+// 		},
+// 		{
+// 			name: "user not found",
+// 			args: args{
+// 				ctx: context.Background(),
+// 				req: &pb.LoginRequest{
+// 					Email:    "not_found@example.com",
+// 					Password: "password",
+// 				},
+// 			},
+// 			mockFunc: func(m *repository.MockUsersRepository) {
+// 				m.EXPECT().GetByEmailAndPassword(context.Background(), "not_found@example.com", mock.AnythingOfType("string")).Return(nil, gorm.ErrRecordNotFound).Times(1)
+// 			},
+// 			assertFunc: func(resp *pb.LoginResponse, err error) {
+// 				s.Assert().ErrorIs(err, &mErr.Err400InvalidLoginInfo)
+// 			},
+// 		},
+// 		{
+// 			name: "success",
+// 			args: args{
+// 				ctx: context.Background(),
+// 				req: &pb.LoginRequest{
+// 					Email:    "success@example.com",
+// 					Password: "password",
+// 				},
+// 			},
+// 			mockFunc: func(m *repository.MockUsersRepository) {
+// 				m.EXPECT().GetByEmailAndPassword(context.Background(), "success@example.com", mock.AnythingOfType("string")).Return(&entity.User{
+// 					ID:       uuid.New(),
+// 					Email:    "success@example.com",
+// 					Password: "password",
+// 					Name:     "test",
+// 				}, nil).Times(1)
+// 			},
+// 			assertFunc: func(resp *pb.LoginResponse, err error) {
+// 				s.Assert().NoError(err)
+// 				s.Assert().Equal("success@example.com", resp.Email)
+// 				s.Assert().Equal("test", resp.Name)
+// 				s.Assert().NotEmpty(resp.Token)
+// 				s.Assert().NotEmpty(resp.ExpiresIn)
+// 			},
+// 		},
+// 	}
+
+// 	for _, tt := range tests {
+
+// 		s.Run(tt.name, func() {
+
+// 			tt.mockFunc(s.mockUserRepo)
+// 			resp, err := s.userService.Login(tt.args.ctx, tt.args.req)
+// 			tt.assertFunc(resp, err)
+// 		})
+// 	}
+// }
